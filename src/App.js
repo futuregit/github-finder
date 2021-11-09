d0a82ff2ad0876dc49c7f15fd3a726ee3494c80b
futuregit <gmail.com>
2021-11-09 03:02:24 -0500

    Added user array

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Component } from 'react';
 import './App.css'
 
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users.js'
 
 class App extends Component {
 
   render(){  
     return (
       <div className="App">
-        <Navbar title="Github Finder" />
+        <Navbar title={"123"}/>
+        <Users />
       </div>
     );
   } 
