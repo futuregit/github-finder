92824406b84b3aa4fac3fe6c5bcfc52f633901f9
futuregit <gmail.com>
2021-11-15 03:47:45 -0500

    Added Repos Component

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ import PropsType from 'prop-types';
 class App extends Component {
   state = {
     users: [],
-    user: {},
+    user: {},    
+    repos: [],
     loading: false,
     alert: null
   }
@@ -42,6 +43,15 @@ class App extends Component {
     this.setState({ user: res.data, loading: false })
   };
 
+  //Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true});
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false })
+  };
+
   //Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -53,7 +63,7 @@ class App extends Component {
   }
 
   render(){
-    const {loading, users, user} = this.state;
+    const {loading, users, user, repos} = this.state;
     
     return (
       <Router>
@@ -78,7 +88,9 @@ class App extends Component {
                 <User 
                   {...props} 
                   getUser={this.getUser} 
-                  user={user} 
+                  getUserRepos={this.getUserRepos} 
+                  user={user}
+                  repos={repos} 
                   loading={loading}
                 />
               )} />
